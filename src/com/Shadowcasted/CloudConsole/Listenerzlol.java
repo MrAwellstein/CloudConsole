6a28ea1bfa79c78a4da7ed0ed1e04805be50a8fb
A. Wellstein <apollowellstein@gmail.com>
2014-01-26 12:18:39 -0600

    Remove Unneeded Debug
    This made sure that the onChat event worked, but after proving that it
    worked, this lost its purpose.

--- src/com/Shadowcasted/CloudConsole/Listenerzlol.java
+++ src/com/Shadowcasted/CloudConsole/Listenerzlol.java
@@ -9,7 +9,6 @@ public class Listenerzlol implements Listener {
 	@EventHandler
 	public void onChat(AsyncPlayerChatEvent chat){
 		try{
-			System.out.println("Bla");
 			Utilities2.say2Chatz("[Chat] "+"<"+chat.getPlayer().getName().toString()+"> "+chat.getMessage()+"]");
 		}catch(Exception e){}
 	}
